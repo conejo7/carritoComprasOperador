7449d0c917de471fea00ac9e27db7f76ad7eaeb3
conejo7 <erick_s5490@hotmail.com>
2023-05-30 22:48:30 -0500

    conectado a eureka

--- src/main/java/com/unir/carritocomprasoperador/controller/PedidoController.java
+++ src/main/java/com/unir/carritocomprasoperador/controller/PedidoController.java
@@ -6,12 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+
+
 
 @RestController
 @RequiredArgsConstructor
