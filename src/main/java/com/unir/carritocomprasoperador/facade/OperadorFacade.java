6b042abcc93932d0cdc00713f6b387c19d86392e
Erick Zambrano <712020:22871663-c02b-442f-b9fb-a4d917659336@bots.bitbucket.org>
2023-06-07 15:45:11 -0500

    fix

--- src/main/java/com/unir/carritocomprasoperador/facade/OperadorFacade.java
+++ src/main/java/com/unir/carritocomprasoperador/facade/OperadorFacade.java
@@ -6,9 +6,8 @@ import com.unir.carritocomprasoperador.model.response.ResponseProductSimple;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import netscape.javascript.JSObject;
-//import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpEntity;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -29,23 +28,17 @@ public class OperadorFacade {
 
     private final RestTemplate restTemplate;
 
-//    public ResponseProductSimple getPedido(String id, ResponseProductSimple responseProductSimple) {
-//
-//        try {
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setContentType(MediaType.APPLICATION_JSON);
-//                //HttpEntity<Product> entity = new HttpEntity<Product>(product,headers);
-////                return restTemplate.exchange(
-////                        "http://localhost:8080/products/"+id, HttpMethod.PUT, entity, String.class).getBody();
-//            HttpEntity<ResponseProductSimple> entity = new HttpEntity<>(ResponseProductSimple,headers);
-//           // System.out.println(restTemplate.getForObject(String.format(getProductUrl, id), ResponseProductSimple.class));
-//            restTemplate.exchange("http://localhost:8080/products/"+id, HttpMethod.PUT, entity, String.class).getBody());
-//            return restTemplate.exchange(String.format(getProductUrl, id), HttpMethod.PUT, entity, ResponseProductSimple.class);
-//        } catch (HttpClientErrorException e) {
-//            log.error("Client Error: {}, Product with ID {}", e.getStatusCode(), id);
-//            return null;
-//        }
-//    }
+    public ResponseProductSimple getPedido(String id) {
+
+        try {
+
+            System.out.println(restTemplate.getForObject(String.format(addProductUrl, id), ResponseProductSimple.class));
+            return restTemplate.getForObject(String.format(addProductUrl, id), ResponseProductSimple.class);
+        } catch (HttpClientErrorException e) {
+            log.error("Client Error: {}, Product with ID {}", e.getStatusCode(), id);
+            return null;
+        }
+    }
 
 
     public ResponseProductSimple minusAmountProduct(String id) {
